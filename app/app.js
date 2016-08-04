2968b961d5ae6973868824bba946b593e8051431
Gareth Paul Jones <gpj@foursquare.com>
2016-08-03 20:00:00 -0600

    fix pointer

--- app/app.js
+++ app/app.js
@@ -5,6 +5,10 @@ const express = require('express');
 const app = express();
 const path = require("path");
 
+// Utilize middleware for for serving files from public dir via /static
+app.use('/static', express.static('public'));
+
+// Serve Index
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
