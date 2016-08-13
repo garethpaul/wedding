d39bb9aacccc1ce00c2b366297eb834b07e0b631
Gareth Paul Jones <gpj@foursquare.com>
2016-08-12 20:00:00 -0600

    Modifying to add swig

--- app/app.js
+++ app/app.js
@@ -4,13 +4,19 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const swig  = require('swig');
 
 // Utilize middleware for for serving files from public dir via /static
 app.use('/static', express.static('public'));
 
 // Serve Index
 app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  var template = swig.compileFile(path.join(__dirname+'/public/index.html'));
+  var output = template({
+      title: 'Kristine + Gareth',
+      authors: ['Paul', 'Jim', 'Jane']
+  });
+  res.send(200, output);
 });
 
 // Start the server
